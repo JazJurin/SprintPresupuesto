47a78c5285c7fa1c99ab55d313531c59909959e3
JazJurin <jazjurin@gmail.com>
2023-10-16 11:59:45 +0200

    Nivel 1 Finalizado

--- src/Components/BudgetList.tsx
+++ src/Components/BudgetList.tsx
@@ -7,7 +7,7 @@ export default function BudgetList({ selectedService, total }) {
 	const [clientName, setClientName] = useState("");
 
 	const addBudget = () => {
-		if (budgetName && clientName) {
+        if (budgetName && clientName) {
 			const newBudget = {
 				name: budgetName,
 				client: clientName,
@@ -27,7 +27,7 @@ export default function BudgetList({ selectedService, total }) {
 	};
 
 	return (
-		<div className="bg-white rounded-lg shadow-lg p-4 m-4 max-w-sm mx-auto ">
+		<div className="bg-white rounded-lg shadow-lg p-4 m-4 max-w-sm mx-auto text-center">
 			<h1 className="text-2xl mb-2">Solicitar Presupuesto</h1>		
 			<input
 				className="input input-bordered join-item m-2 bg-slate-300"
@@ -44,10 +44,12 @@ export default function BudgetList({ selectedService, total }) {
 			<button onClick={addBudget} className="btn btn-outline m-2">
 				Agregar Presupuesto
             </button>
-            <h2 className="text-2xl mb-2">Lista de presupuestos:</h2>
+            
             <ul className="list-item p-0">
-				{budgets.map((budget, index) => (
-					<li key={index} className="mb-4">
+                {budgets.map((budget, index) => (
+                    <>
+                    <h2 className="text-2xl mb-2">Lista de presupuestos:</h2>
+					<li key={index} className="m-4 text-left">
 						<div>
 							<strong>Nombre del Presupuesto:</strong> {budget.name}
 						</div>
@@ -64,7 +66,8 @@ export default function BudgetList({ selectedService, total }) {
 							<strong>Total:</strong> {budget.total} €
 						</div>
 						
-					</li>
+                        </li>
+                        </>
 				))}
 			</ul>
 		</div>
